a6ab385cced2c594a891f54598e36fe5f51cfbfa
Augusto Maza <debianmaza@gmail.com>
2020-11-05 19:15:26 -0300

    fix script7

--- sqls/dump_data_base.sh
+++ sqls/dump_data_base.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 fecha=`date +%d-%m-%Y`
 archivo="siadcon-$fecha.sql"
-mysqldump --user=siadcon --password=siadcon siadcon > $archivo
+sudo mysqldump --user=siadcon --password=siadcon siadcon > $archivo
 
 echo -e "Database Backing Up Successfully \n File storage Successfully"
 
